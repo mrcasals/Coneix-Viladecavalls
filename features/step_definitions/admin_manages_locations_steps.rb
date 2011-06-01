6bcc9a31800403a4147a9ed56940ed076b633fb0
mrc2407 <mrc2407@gmail.com>
2011-06-01 18:16:35 +0200

    Start with steps

---
+++ features/step_definitions/admin_manages_locations_steps.rb
@@ -0,0 +1,56 @@
+Given /^I am logged in as an admin$/ do
+  @current_user = User.make!(:admin)
+  login_as @current_user
+end
+
+Given /^a location exists$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^I am on the location creation page$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^I am on the location page$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I am on the locations list$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I fill in the form with the location data$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I modify the location data$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I delete a location$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see the location$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see the location within the locations list$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see a confirmation message telling me that the data was successfully updated$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see the modifications in the location page$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see a confirmation message telling me that the location was successfully deleted$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should not see the location within the locations list$/ do
+  pending # express the regexp above with the code you wish you had
+end
