e78e1c3c2529e367d10b087d23ad00b7aa772eef
mrc2407 <mrc2407@gmail.com>
2011-06-01 16:08:09 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Patrimonivila::Application.config.secret_token = 'f32ecf5e9fd7982a6b442cca51c0b5b0741799dda1b06e9f1fd960a7f68d734d09baf40a4f18783607db8267ae9a8e651d94865e9c9cbed6dc622a8c62d3b090'
